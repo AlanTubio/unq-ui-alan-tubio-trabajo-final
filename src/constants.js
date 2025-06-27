dc380564ba615460ea6fa5b67d05cab096a199cd
Alan Tubio <alannahuelmiya2@hotmail.com>
2025-06-27 09:17:40 -0300

    bootstrap instalado

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+const API = {
+  BASE_URL: "https://word-api-hmlg.vercel.app",
+};
+
+const API_ROUTES = {
+  DIFFICULTIES: "/api/difficulties",
+  CHECKWORD: "/api/checkWord",
+};
+
+export default { API, API_ROUTES };
